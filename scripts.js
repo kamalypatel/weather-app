896a1e7be2b07c002c826e3819c227eadee49f29
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-06-11 07:10:49 -0400

    added function to get a user input location from the UI

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
-fetch('https://api.weatherapi.com/v1/current.json?key=1b6e3493db6147c3bc5155935242605&q=london', {mode: 'cors'})
+function getForecast (location) {
+    if (location == '') {
+        console.log('No location entered')
+    } else {
+        fetch(`https://api.weatherapi.com/v1/forecast.json?key=1b6e3493db6147c3bc5155935242605&q=${location}&days=7`, {mode: 'cors'})
     .then(function(response) {
        return response.json()
     })
     .then(function(response) {
         console.log(response)
     })
+    displayForecast()
+    }
+}
+
+function displayForecast() {
+    
+}
+
+let locationInput = document.querySelector('.location-input')
+const button = document.querySelector('.location-button')
+button.addEventListener('click', function() {
+    getForecast(locationInput.value)
+})
